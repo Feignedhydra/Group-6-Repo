2f9101664510fbe374469e5db3b09a15c85b617b
Unknown <26903202@student.g.nwu.ac.za>
2017-08-09 12:58:12 +0200

    Fixing spelling mistakes to bio's

--- Git Assignment/Git Assignment/Git Assignment/Armand.cs
+++ Git Assignment/Git Assignment/Git Assignment/Armand.cs
@@ -15,7 +15,7 @@ namespace Git_Assignment
         public Armand()
         {
             InitializeComponent();
-            rTbxBio.Text = "Armand Havenga - South African Citizen. Born on 25 January 1993, in the Free State, in a town called Parys. Attendet preschool, primary(Lareskool H.F Verwoerd) and highschool(Highschool Parys) in parys. However after matriculating in 2011 at Parys Highschool, a extra post year was done by Afrikaans Highschool Kroonstad. Started studying MBW (Menslike Bewegings wetenskap) in 2013 but could not finish as the opertunity to study BSc IT became avaialable. Started with BSc IT course in 2015. Studying are conducted at the North-west University of Potchefstroom (Pukke).";
+            rTbxBio.Text = "Armand Havenga - South African Citizen. Born on 25 January 1993, in the Free State, in a small town called Parys. Attended preschool, primary(Laerskool H.F Verwoerd) and highschool(Highschool Parys). However after matriculating in 2011 at Parys Highschool, a extra post year was done by Afrikaans Highschool Kroonstad. Started studying MBW (Menslike Bewegings wetenskap) in 2013 but could not finish as the opportunity to study BSc IT became avaialable. Started with BSc IT course in 2015. Studying are conducted at the North-west University of Potchefstroom (Pukke).";
         }
     }
 }
